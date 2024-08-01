2ef4889cac5f611dc8b371c175c562780cc4076c
Raven Paragas <ravenparagas@Ravens-MacBook-Pro.local>
2024-08-01 23:55:35 +0800

    alias, require, import and use

---
+++ 13-alias-require-import-and-use.exs
@@ -0,0 +1,23 @@
+require Integer
+
+# Alias the module so it cann be called as Bar instead of Foo.bar
+# alias Foo.Bar, as: Bar
+
+# Require the module in order to use its macros
+# require Foo
+
+# Import functions from Foo so they can be called without the `Foo.` prefix
+# import Foo
+
+# Invokes the custom code defined in Foo as an extension Point
+# use Foo
+
+IO.inspect(Integer.is_odd(3))
+
+defmodule AssertionTest do
+  use ExUnit.Case, async: true
+
+  test "always pass" do
+    assert true
+  end
+end
